09be2bf6888ffd83496439346ad3dab94e67b3c3
DurjoyAcharya <da-durjoy@outlook.com>
2023-06-02 20:05:44 +0600

    done

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * 
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
